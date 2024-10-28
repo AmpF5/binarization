d52d3b96dc78e05fef3e0d6317128e9be9b7ff15
AmpF5 <lolc44t@gmail.com>
2024-10-28 18:40:47 +0100

    init

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace binarization;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application {
+}
---
+++ Scripts/SauvoliBinarization.cs
@@ -0,0 +1,5 @@
+namespace binarization.Scripts;
+
+public class SauvoliBinarization {
+    
+}
