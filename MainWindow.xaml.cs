8169ca9c1de2701251c9a8db6f640cf190918abd
AmpF5 <lolc44t@gmail.com>
2024-10-28 18:53:21 +0100

    add loading image

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,20 +1,25 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
+﻿using System.Windows;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace binarization;
 
 public partial class MainWindow : Window {
     public MainWindow() {
         InitializeComponent();
+    }
+    
+    private void LoadImage_Click(object sender, RoutedEventArgs e) {
+        var openFileDialog = new OpenFileDialog {
+            Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp"
+        };
+
+        if(openFileDialog.ShowDialog() == false) 
+            return;
         
-        
+        var bitmap = new BitmapImage(new Uri(openFileDialog.FileName));
+        DisplayedImage.Source = bitmap;
     }
 }
+
+
