17c4c8349ef85f1e2c6e0c1d220f48ea79e99267
AmpF5 <lolc44t@gmail.com>
2024-11-03 13:41:16 +0100

    niblack fix

--- Scripts/NiblackBinarization.cs
+++ Scripts/NiblackBinarization.cs
@@ -6,51 +6,60 @@ using binarization.Helpers;
 namespace binarization.Scripts;
 
 public static class NiblackBinarization {
-    public static ImageSource Binarize(ImageSource image, int windowSize = 5, double k = 0.5, int r = 128) {
+    public static ImageSource Binarize(ImageSource image, int windowSize = 5, double k = 0.25, int p = 3, int q = 3, double r = 0.5) {
         var stopwatch = Stopwatch.StartNew();
         
         var writeableImage = image.ConvertToWriteableBitmap();
         
-        writeableImage.Lock();
-        
         var width = writeableImage.PixelWidth;
         var height = writeableImage.PixelHeight;
+        var pixelFormatSize = writeableImage.Format.BitsPerPixel / 8;
         var stride = writeableImage.BackBufferStride;
         var pBackBuffer = writeableImage.BackBuffer;
-        
-        var radius = (windowSize - 1) / 2;
-        var blockSize = 2 * radius + 1;
-        var block = new byte[blockSize * blockSize];
+        var grayscaleBuffer = new byte[width * height];
 
-        for (var y = radius; y < height - radius; y++) {
+        var radius = (windowSize - 1) / 2;
+        r *= 256;
+        var blockSize = 2 * radius + 1;
+        
+        writeableImage.Lock();
+        var pBackBufferCopy = writeableImage.BackBuffer;
+        
+        Parallel.For(0, height, y => {
+            for (var x = 0; x < width; x++) {
+                var pPixel = pBackBufferCopy + y * stride + x * pixelFormatSize;
+                var colorData = Marshal.ReadInt32(pPixel);
+
+                var grayscale = ColorHelper.CalculateGrayscale(colorData);
+                grayscaleBuffer[y * width + x] = grayscale;
+            }
+        });
+        
+        var blockLock = new ThreadLocal<byte[]>(() => new byte[blockSize * blockSize]);
+
+        Parallel.For(radius, height - radius, y => {
+            var block = blockLock.Value;
             for (var x = radius; x < width - radius; x++) {
-                
-                Array.Clear(block, 0, block.Length);
-                
                 for (var i = -radius; i <= radius; i++) {
                     for (var j = -radius; j <= radius; j++) {
-                        var pNeighbourPixel = pBackBuffer + (i + radius) * stride + (j + radius) * 4;
-                        var colorNeighbourData = Marshal.ReadInt32(pNeighbourPixel);
-                        
                         var index = (i + radius) * blockSize + (j + radius);
-                        block[index] = ColorHelper.CalculateGrayscale(colorNeighbourData);
+                        block[index] = grayscaleBuffer[(y + i) * width + (x + j)];
                     }
                 }
-                
+
                 var mean = MathHelper.CalculateMean(block);
                 var standardDeviation = MathHelper.CalculateStandardDeviation(block, mean);
-                var threshold = mean + (k * standardDeviation);
+                var threshold = (short) Math.Round(mean * (1 + p * Math.Exp(-q * mean) + k * ((standardDeviation / r) - 1)));
                 
-                var pPixel = pBackBuffer + y * stride + x * 4;
-                var colorData = Marshal.ReadInt32(pPixel);
-                var grayscale = ColorHelper.CalculateGrayscale(colorData);
-                
-                var binaryColor = grayscale < threshold ? (byte)0 : (byte)255;
+                var grayscale = grayscaleBuffer[y * width + x];
+
+                var binaryColor = grayscale < threshold ? (byte) 0 : (byte) 255;
                 var binarizedColorData = (255 << 24) | (binaryColor << 16) | (binaryColor << 8) | binaryColor;
                 
+                var pPixel = pBackBuffer + y * stride + x * 4;
                 Marshal.WriteInt32(pPixel, binarizedColorData);
             }
-        }
+        });
         
         writeableImage.Unlock();
         
