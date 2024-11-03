ce888cdfb6e74923507a7bd31b78e2791cd6d29c
AmpF5 <lolc44t@gmail.com>
2024-11-03 12:24:00 +0100

    sauvola fix

--- Scripts/SauvolaBinarization.cs
+++ Scripts/SauvolaBinarization.cs
@@ -1,4 +1,3 @@
-
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Windows.Media;
@@ -8,56 +7,64 @@ namespace binarization.Scripts;
 
 public static class SauvolaBinarization {
     public static ImageSource Binarize(ImageSource image, int windowSize = 5, double k = 0.5, int r = 128) {
-        var stopwatch = Stopwatch.StartNew();
-        
+       var stopwatch = Stopwatch.StartNew();
         var writeableImage = image.ConvertToWriteableBitmap();
-        
-        writeableImage.Lock();
-        
+
         var width = writeableImage.PixelWidth;
         var height = writeableImage.PixelHeight;
         var stride = writeableImage.BackBufferStride;
-        var pBackBuffer = writeableImage.BackBuffer;
+        var pixelFormatSize = writeableImage.Format.BitsPerPixel / 8;
         
+        var pBackBuffer = writeableImage.BackBuffer;
         var radius = (windowSize - 1) / 2;
         var blockSize = 2 * radius + 1;
-        var block = new byte[blockSize * blockSize];
+        
+        var grayscaleBuffer = new byte[width * height];
+        
+        writeableImage.Lock();
+        var pBackBufferCopy = writeableImage.BackBuffer;
+        
+        Parallel.For(0, height, y => {
+            for (var x = 0; x < width; x++) {
+                var pPixel = pBackBufferCopy + y * stride + x * pixelFormatSize;
+                var colorData = Marshal.ReadInt32(pPixel);
 
-        for (var y = radius; y < height - radius; y++) {
+                var grayscale = ColorHelper.CalculateGrayscale(colorData);
+                grayscaleBuffer[y * width + x] = grayscale;
+            }
+        });
+
+        var blockLock = new ThreadLocal<byte[]>(() => new byte[blockSize * blockSize]);
+
+        
+        Parallel.For(radius, height - radius, y => {
+            var block = blockLock.Value;
             for (var x = radius; x < width - radius; x++) {
-                
-                Array.Clear(block, 0, block.Length);
-                
                 for (var i = -radius; i <= radius; i++) {
                     for (var j = -radius; j <= radius; j++) {
-                        var pNeighbourPixel = pBackBuffer + (i + radius) * stride + (j + radius) * 4;
-                        var colorNeighbourData = Marshal.ReadInt32(pNeighbourPixel);
-                        
                         var index = (i + radius) * blockSize + (j + radius);
-                        block[index] = ColorHelper.CalculateGrayscale(colorNeighbourData);
+                        block[index] = grayscaleBuffer[(y + i) * width + (x + j)];
                     }
                 }
-                
+
                 var mean = MathHelper.CalculateMean(block);
                 var standardDeviation = MathHelper.CalculateStandardDeviation(block, mean);
                 var threshold = mean * (1 + k * ((standardDeviation / r) - 1));
                 
-                var pPixel = pBackBuffer + y * stride + x * 4;
-                var colorData = Marshal.ReadInt32(pPixel);
-                var grayscale = ColorHelper.CalculateGrayscale(colorData);
-                
-                var binaryColor = grayscale < threshold ? (byte)0 : (byte)255;
+                var grayscale = grayscaleBuffer[y * width + x];
+
+                var binaryColor = grayscale < threshold ? (byte) 0 : (byte) 255;
                 var binarizedColorData = (255 << 24) | (binaryColor << 16) | (binaryColor << 8) | binaryColor;
-                
+
+                var pPixel = pBackBuffer + y * stride + x * pixelFormatSize;
                 Marshal.WriteInt32(pPixel, binarizedColorData);
             }
-        }
-        
-        writeableImage.Unlock();
-        
+        });
+
         var timeTaken = stopwatch.Elapsed;
         Console.WriteLine("Time taken: " + timeTaken.ToString(@"m\:ss\.fff"));
-        
+            
+        writeableImage.Unlock();
         return writeableImage;
     }
 }
