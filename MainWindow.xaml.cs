620f661d73654b912397777759179d8eeef79148
AmpF5 <lolc44t@gmail.com>
2024-11-06 23:20:16 +0100

    UI refactor

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.IO;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using binarization.Scripts;
@@ -10,7 +9,12 @@ using Microsoft.Win32;
 namespace binarization;
 
 public partial class MainWindow : Window {
-    private ImageSource _originalImage = null!; 
+    private ImageSource _originalImage = null!;
+    private int _windowSize = 5;
+    private BinarizationType _binarizationType = BinarizationType.Threshold;
+    private Button _selectedButton;
+    private const int MinValue = 0;
+    private const int MaxValue = 25;
     public MainWindow() {
         InitializeComponent();
     }
@@ -32,43 +36,90 @@ public partial class MainWindow : Window {
         DisplayedImage.Source = _originalImage;
     }
 
-    private void Apply_Binarization(object sender, RoutedEventArgs e) {
+    private void Choose_Binarization(object sender, RoutedEventArgs e) {
+        WindowSizeStackPanel.Visibility = Visibility.Visible;
         var button = sender as Button;
 
         if(button?.CommandParameter is not BinarizationType type) 
             return;
         
-        try {
-            switch (type) {
-                case BinarizationType.Threshold:
-                    SliderStackPanel.Visibility = Visibility.Visible;
-                    break;
-                case BinarizationType.Sauvola:
-                    DisplayedImage.Source = SauvolaBinarization.Binarize(_originalImage);
-                    break;
-                case BinarizationType.Phansalkara:
-                    DisplayedImage.Source = PhansalkaraBinarization.Binarize(_originalImage);
-                    break;
-                case BinarizationType.Otsu:
-                    DisplayedImage.Source = OtsuBinarization.Binarize(_originalImage);
-                    break;
-                case BinarizationType.Niblack:
-                    DisplayedImage.Source = NiblackBinarization.Binarize(_originalImage);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException($"Type {type} not found");
-            }
+        _binarizationType = type;
+        switch (_binarizationType) {
+            case BinarizationType.Otsu:
+                NumberTextBox.Visibility = Visibility.Collapsed;
+                break;
+            case BinarizationType.Threshold:
+                NumberTextBox.Visibility = Visibility.Visible;
+                break;
+            case BinarizationType.Sauvola:
+            case BinarizationType.Phansalkara:
+            case BinarizationType.Niblack:
+            default:
+                NumberTextBox.Visibility = Visibility.Visible;
+                break;
         }
-        catch (Exception ex) {
-            Console.WriteLine(ex);
+        
+        var clickedButton = sender as Button;
+
+        if (_selectedButton != null)
+        {
+            _selectedButton.ClearValue(BorderBrushProperty);
+            _selectedButton.ClearValue(BorderThicknessProperty);
+        }
+        
+        if (clickedButton != null)
+        {
+            clickedButton.BorderBrush = Brushes.Blue;
+            clickedButton.BorderThickness = new Thickness(2);
+            _selectedButton = clickedButton;
         }
     }
     
-    private void ThresholdSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
-        if (sender is not Slider slider || CurrentThresholdValue == null) 
-            return;
-        
-        CurrentThresholdValue.Text = $"Current Value: {slider.Value:F0}";
-        DisplayedImage.Source = ThresholdBinarization.Binarize(_originalImage, (byte)slider.Value);
+    private void NumberValidationTextBox(object sender, TextCompositionEventArgs e) {
+        var textBox = sender as TextBox;
+        var newText = textBox.Text.Insert(textBox.SelectionStart, e.Text);
+
+        if (int.TryParse(newText, out var result))
+        {
+            if (result < MinValue || result > MaxValue)
+            {
+                e.Handled = true;
+            }
+        }
+        else
+        {
+            e.Handled = true;
+        }
+    }
+
+    private void TextChangeTextBox(object sender, TextChangedEventArgs e) {
+        var textBox = sender as TextBox;
+        if(int.TryParse(textBox.Text, out var result)) {
+            if (result < MinValue) {
+                textBox.Text = MinValue.ToString();
+                textBox.CaretIndex = textBox.Text.Length;
+            }
+            else if (result > MaxValue) {
+                textBox.Text = MaxValue.ToString();
+                textBox.CaretIndex = textBox.Text.Length;
+            }
+
+            _windowSize = int.Parse(textBox.Text);
+        }
+        else {
+            textBox.Text = string.Empty;
+            _windowSize = 0;
+        }
+    }
+
+    private void Apply_Binarization(object sender, RoutedEventArgs e) {
+        DisplayedImage.Source = _binarizationType switch {
+            BinarizationType.Threshold => ThresholdBinarization.Binarize(_originalImage, threshold: (byte)_windowSize),
+            BinarizationType.Sauvola => SauvolaBinarization.Binarize(_originalImage, windowSize: _windowSize),
+            BinarizationType.Phansalkara => PhansalkaraBinarization.Binarize(_originalImage, windowSize: _windowSize),
+            BinarizationType.Otsu => OtsuBinarization.Binarize(_originalImage),
+            BinarizationType.Niblack => NiblackBinarization.Binarize(_originalImage, windowSize: _windowSize),
+            _ => throw new ArgumentOutOfRangeException($"Type {_binarizationType} not found")
+        };
     }
 }
